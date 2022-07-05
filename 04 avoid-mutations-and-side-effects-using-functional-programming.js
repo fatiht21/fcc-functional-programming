e94a4ca2d9f49a9e6fee965bdcffdb664b831112
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-07-05 09:39:22 +0200

    avoid-mutations-and-side-effects-using-functional-programming

---
+++ 04 avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,15 @@
+/*
+Fill in the code for the function incrementer so it returns the value of the global variable fixedValue increased by one.
+
+Your function incrementer should not change the value of fixedValue (which is 4).
+Your incrementer function should return a value that is one larger than the fixedValue value.
+Your incrementer function should return a value based on the global fixedValue variable value.
+*/
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+  return fixedValue + 1;
+  // Only change code above this line
+}
