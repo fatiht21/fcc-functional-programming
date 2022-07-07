317a0247ffaba1cecdf1a66328f925c26c393837
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-07-07 18:12:43 +0200

    implement-the-filter-method-on-a-prototype

---
+++ 10 implement-the-filter-method-on-a-prototype.js
@@ -0,0 +1,24 @@
+/*
+Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter(). You should not use the built-in filter method. The Array instance can be accessed in the myFilter method using this.
+
+new_s should equal [23, 65, 5].
+Your code should not use the filter method
+*/
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback) {
+  // Only change code below this line
+  const newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i]) == true){
+      newArray.push(this[i]);
+    }    
+  }
+  // Only change code above this line
+  return newArray;
+};
+
+const new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
+});
