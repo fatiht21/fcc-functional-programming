a3af91c1a0aa45e067cf05a6dc7089f414ee3a1e
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-07-07 18:13:06 +0200

    implement-the-filter-method-on-a-prototype

--- 10 implement-the-filter-method-on-a-prototype.js
+++ 10 implement-the-filter-method-on-a-prototype.js
@@ -7,18 +7,18 @@ Your code should not use the filter method
 // The global variable
 const s = [23, 65, 98, 5];
 
-Array.prototype.myFilter = function(callback) {
+Array.prototype.myFilter = function (callback) {
   // Only change code below this line
   const newArray = [];
   for (let i = 0; i < this.length; i++) {
-    if (callback(this[i]) == true){
+    if (callback(this[i]) == true) {
       newArray.push(this[i]);
-    }    
+    }
   }
   // Only change code above this line
   return newArray;
 };
 
-const new_s = s.myFilter(function(item) {
+const new_s = s.myFilter(function (item) {
   return item % 2 === 1;
 });
